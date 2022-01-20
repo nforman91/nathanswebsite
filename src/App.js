a0ab7d71875bda859797cb7ec6be264785b6cd97
Nathan <nathan.forman@gmail.com>
2022-01-19 21:38:17 -0800

    Started about us page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
+import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
     <div className="App">
-      <h1>Film</h1>
-      <h2>Blah</h2>
+      <AboutUs />
     </div>
   );
 }
