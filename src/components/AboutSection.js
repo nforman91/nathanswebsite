9212a1f59b7c546837b2799674041eab45b26a63
Nathan <nathan.forman@gmail.com>
2022-01-21 15:44:43 -0800

    Styled components started

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,34 +1,68 @@
 import React from "react";
 import skiingnate from "../img/skiingnate.JPG";
+// Styled
+import styled from 'styled-components';
 
 const AboutSection = () => {
   return (
-    <div>
-      <div className="description">
+    <StyledAbout>
+      <StyledDescription>
         <div className="title">
-          <div className="hide">
+          <Hide>
             <h2>We work to make</h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>
               your <span>dreams</span> come
             </h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>true.</h2>
-          </div>
+          </Hide>
         </div>
         <p>
           Contact us for any photography or videography ideas that you have. We
           have professionals with amazing skills.
         </p>
         <button>Contact Us</button>
-      </div>
-      <div className="image">
+      </StyledDescription>
+      <StyledImage>
         <img src={skiingnate} alt="guy with a camera" />
-      </div>
-    </div>
+      </StyledImage>
+    </StyledAbout>
   );
 };
+
+// Styled Components
+const StyledAbout = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+`;
+
+const StyledDescription = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2{
+    font-weight: lighter;
+  }
+`;
+
+const StyledImage = styled.div`
+  flex: 1;
+  overflow: hidden;
+  img{
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
 
 export default AboutSection;
