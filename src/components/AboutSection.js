fa9d9860c13907c5e29222c4a739eb6fa0c9c8b9
Nathan <nathan.forman@gmail.com>
2022-01-21 16:01:02 -0800

    Styled services section

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -2,6 +2,7 @@ import React from "react";
 import skiingnate from "../img/skiingnate.JPG";
 // Styled
 import styled from 'styled-components';
+import { StyledAbout, StyledDescription, StyledImage, Hide } from '../styles';
 
 const AboutSection = () => {
   return (
@@ -34,35 +35,5 @@ const AboutSection = () => {
 };
 
 // Styled Components
-const StyledAbout = styled.div`
-  min-height: 90vh;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 5rem 10rem;
-  color: white;
-`;
-
-const StyledDescription = styled.div`
-  flex: 1;
-  padding-right: 5rem;
-  h2{
-    font-weight: lighter;
-  }
-`;
-
-const StyledImage = styled.div`
-  flex: 1;
-  overflow: hidden;
-  img{
-    width: 100%;
-    height: 80vh;
-    object-fit: cover;
-  }
-`;
-
-const Hide = styled.div`
-  overflow: hidden;
-`;
 
 export default AboutSection;
--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -1,49 +1,80 @@
 import React from "react";
 import stopwatch from "../img/stopwatch.png";
+import { StyledAbout, StyledDescription, StyledImage } from '../styles';
+import styled from 'styled-components';
 
 const ServicesSection = () => {
   return (
-    <div className="services">
-      <div className="description">
+    <Services>
+      <StyledDescription>
         <h2>
           High <span>quality</span> services
         </h2>
-        <div className="cards">
-          <div className="card">
+        <Cards>
+          <Card>
             <div className="icon">
               <img src={stopwatch} alt="stopwatch"/>
               <h3>Efficient</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div className="card">
+          </Card>
+          <Card>
             <div className="icon">
               <img src={stopwatch} alt="stopwatch"/>
               <h3>Teamwork</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div className="card">
+          </Card>
+          <Card>
             <div className="icon">
               <img src={stopwatch} alt="stopwatch"/>
               <h3>Diaphragm</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div className="card">
+          </Card>
+          <Card>
             <div className="icon">
               <img src={stopwatch} alt="stopwatch"/>
               <h3>Affordable</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-        </div>
-      </div>
-      <div className="image">
+          </Card>
+        </Cards>
+      </StyledDescription>
+      <StyledImage>
         <img src={stopwatch} alt="stopwatch"/>
-      </div>
-    </div>
+      </StyledImage>
+    </Services>
   );
 };
+
+const Services = styled(StyledAbout)`
+  h2{
+    padding-bottom: 5rem;
+  }
+  p{
+    width: 70%;
+    padding: 2rem 0rem 4rem 0rem;
+  }
+`;
+
+const Cards = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Card = styled.div`
+  flex-basis: 20rem;
+  .icon{
+    display: flex;
+    align-items: center;
+    h3{
+      margin-left: 1rem;
+      background: white;
+      color: black;
+      padding: 1rem;
+    }
+  }
+`;
 
 export default ServicesSection;
