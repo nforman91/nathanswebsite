0951bb8e7947c08a83c16c71e9ea65e1ae062f3f
Nathan <nathan.forman@gmail.com>
2022-01-25 22:34:02 -0800

    Got nav line animation and done with tutorial now to make my own

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+import { useLocation } from 'react-router-dom';
 
 const Nav = () => {
+    const { pathname } = useLocation();
     return (
         <StyledNav>
             <h1><Link id="logo" to="/">
@@ -11,12 +14,27 @@ const Nav = () => {
             <ul>
                 <li>
                     <Link to="/">1. About Us</Link>
+                    <StyledLine 
+                        transition={{ duration: 0.75 }} 
+                        initial={{ width: "0%" }}
+                        animate={{ width: pathname === "/" ? "50%" : "0%" }}
+                    />
                 </li>
                 <li>
                     <Link to="/work">2. Our Work</Link>
+                    <StyledLine 
+                        transition={{ duration: 0.75 }} 
+                        initial={{ width: "0%" }}
+                        animate={{ width: pathname === "/work" ? "50%" : "0%" }}
+                    />
                 </li>
                 <li>
                     <Link to="/contact">3. Contact Us</Link>
+                    <StyledLine 
+                        transition={{ duration: 0.75 }} 
+                        initial={{ width: "0%" }}
+                        animate={{ width: pathname === "/contact" ? "50%" : "0%" }}
+                    />
                 </li>
             </ul>
         </StyledNav>
@@ -66,6 +84,18 @@ const StyledNav = styled.nav`
                 padding: 0;
             }
         }
+    }
+`;
+
+const StyledLine = styled(motion.div)`
+    height: 0.3rem;
+    background: #23d997;
+    width: 0%;
+    position: absolute;
+    bottom: -80%;
+    left: 60%;
+    @media (max-width: 320px){
+        left: 0%;
     }
 `;
 
